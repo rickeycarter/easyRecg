b11096e8b43d99e149b37c992915549b332d7673
Rickey Carter <carter.rickey@mayo.edu>
2021-08-05 16:24:40 -0500

    Misc function

---
+++ R/read_muse_xml_ecg.R
@@ -0,0 +1,11 @@
+#' Read a single XML representation of an ECG exported from MUSE. Returns an array with leads and amplitude values.
+#'
+#' @param filename  Filename for the xml file that will be read
+#' @export
+#' @examples
+#' ecg_1 <- read_muse_xml_ecg("1_MUSSE_ecg.xml")
+
+read_muse_xml_ecg <- function(filename){
+  
+  
+}
