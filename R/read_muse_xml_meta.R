d09fddc08947ee6b26e41b61442e5b739e96d945
overdodactyl <patjohns1216@gmail.com>
2021-08-15 15:41:06 -0500

    feat: add include/exclude params to read_muse_xml_meta

--- R/read_muse_xml_meta.R
+++ R/read_muse_xml_meta.R
@@ -1,10 +1,15 @@
 #' Extract meta data from MUSE XML files
 #'
 #' @param file Either a path to a file or a collection of file paths
-#'
+#' @param include A named list of data frames and columns to return.
+#'     If a data frame is listed with it's element being NA, all columns are returned for that data frame.  
+#'     `include` is ran before `exclude` if both are provided.  
+#' @param exclude A named list of data frames and columns to exclude.
+#'    If a data frame is listed with it's element being NA, the entire dataframe is removed. 
+#'    `include` is ran before `exclude` if both are provided.
 #' @return A list of data frames (or tibbles)
 #' @export
-read_muse_xml_meta <- function(file) {
+read_muse_xml_meta <- function(file, include = NULL, exclude = NULL) {
 
   file <- unique(file)
   
@@ -14,7 +19,54 @@ read_muse_xml_meta <- function(file) {
   
   parsed_list <- purrr::map(file, parse_meta)
   transposed <- purrr::transpose(parsed_list)
-  purrr::map(transposed, dplyr::bind_rows)
+  complete_data <- purrr::map(transposed, dplyr::bind_rows)
+  
+  
+  if (!is.null(include)) {
+    if (is.null(names(include))) {
+      stop("include must be a named list")
+    }
+    
+    datasets <- names(include)
+    requested_not_found <- setdiff(datasets, names(complete_data))
+    requested_found <- datasets[datasets %in% names(complete_data)]
+    
+    for (d in requested_not_found) {
+      warning(paste0("include dataset not found and ignored: ", d))
+    }
+    
+    complete_data <- complete_data[requested_found]
+    
+    for (n in requested_found) {
+      if (!identical(include[[n]], NA)) {
+        complete_data[[n]] <- dplyr::select(complete_data[[n]], tidyselect::any_of(include[[n]]))
+      }
+    }
+  }
+  
+  if (!is.null(exclude)) {
+    # Must be a named list
+    if (is.null(names(exclude))) {
+      stop("exclude must be a named list")
+    }
+    for (n in names(exclude)) {
+      if (!n %in% names(complete_data)) {
+        warning(paste0(n, " is not a dataset returned.  Entry has been ignored."))
+      } else {
+        if (is.na(exclude[[n]])) {
+          complete_data[[n]] <- NULL
+        } else {
+          complete_data[[n]] <- dplyr::select(complete_data[[n]], -tidyselect::any_of(exclude[[n]]))
+        }
+      }
+    }
+  }
+  
+  
+  complete_data
+  
+  
+  
 }
 
 parse_meta <- function(file) {
